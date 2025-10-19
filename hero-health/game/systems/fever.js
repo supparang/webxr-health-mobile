7c6bf220929ad21e729c86c55432ff4b25ae210d
supparang <supparang.r@chandra.ac.th>
2025-10-19 21:02:12 +0700

    Update fever.js

--- hero-health/game/systems/fever.js
+++ hero-health/game/systems/fever.js
@@ -1,1 +1,20 @@
-export class FeverSystem{ constructor(){ this.timer=0; this.active=false; } scoreMul(){ return this.active?2:1; } update(dt){ if(this.active){ this.timer-=dt; if(this.timer<=0){ this.active=false; } } } onBad(){ this.active=false; } }
+export class FeverSystem{
+  constructor(){
+    this.timer = 0;
+    this.active = false;
+  }
+  scoreMul(){
+    return this.active ? 2 : 1;
+  }
+  update(dt){
+    if(this.active){
+      this.timer -= dt;
+      if(this.timer <= 0){
+        this.active = false;
+      }
+    }
+  }
+  onBad(){
+    this.active = false;
+  }
+}
