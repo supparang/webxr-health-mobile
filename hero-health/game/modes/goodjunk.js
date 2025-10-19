b3d4f1368c75c466563d576de5c00950afa90857
supparang <supparang.r@chandra.ac.th>
2025-10-19 20:57:55 +0700

    Update goodjunk.js

--- hero-health/game/modes/goodjunk.js
+++ hero-health/game/modes/goodjunk.js
@@ -1,1 +1,17 @@
-export const name='ดี vs ขยะ'; const goods=['🥦','🍎','🍇','🥕','🍅','🌽','🥚']; const junks=['🍔','🍟','🍕','🥤','🍩']; export function pickMeta(diff,state){ const good=Math.random()<0.6; const char=good?goods[Math.floor(Math.random()*goods.length)]:junks[Math.floor(Math.random()*junks.length)]; return {type:'gj',good,char}; } export function onHit(meta,systems){ if(meta.good){ systems.score.add(5);} else{ systems.score.add(-2);} }
+export const name = 'ดี vs ขยะ';
+
+const goods = ['🥦','🍎','🍇','🥕','🍅','🌽','🥚'];
+const junks = ['🍔','🍟','🍕','🥤','🍩'];
+
+export function pickMeta(diff, state){
+  const good = Math.random() < 0.6;
+  const char = good
+    ? goods[Math.floor(Math.random()*goods.length)]
+    : junks[Math.floor(Math.random()*junks.length)];
+  return { type:'gj', good, char };
+}
+
+export function onHit(meta, systems){
+  if(meta.good){ systems.score.add(5); }
+  else{ systems.score.add(-2); }
+}
