440ceb2a6ba94f75525b952b3eeea79b4f2564f3
supparang <supparang.r@chandra.ac.th>
2025-10-26 15:16:20 +0700

    Update score.js

--- HeroHealth/game/core/score.js
+++ HeroHealth/game/core/score.js
@@ -1,6 +1,22 @@
-// game/core/score.js
-export class ScoreSystem{
-  constructor(){ this.score=0; this.combo=0; }
-  reset(){ this.score=0; this.combo=0; }
-  add(n){ this.score += (n|0); }
+// core/score.js
+// เก็บคะแนน + hook เผื่อในอนาคต
+
+export class ScoreSystem {
+  constructor() {
+    this.score = 0;
+    this._handlers = { change: null };
+    this._boostFn = null;
+  }
+  reset() {
+    this.score = 0;
+    this._emit();
+  }
+  add(n) {
+    const extra = this._boostFn ? (this._boostFn(n)|0) : 0;
+    this.score = (this.score|0) + (n|0) + extra;
+    this._emit();
+  }
+  setBoostFn(fn) { this._boostFn = typeof fn === 'function' ? fn : null; }
+  setHandlers(h = {}) { this._handlers = { ...this._handlers, ...h }; }
+  _emit() { try { this._handlers.change?.(this.score); } catch {} }
 }
