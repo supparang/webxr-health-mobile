29e0a7de5dc0d8aa85e8c6682ef759eacdf3379a
supparang <supparang.r@chandra.ac.th>
2025-11-01 15:20:06 +0700

    Update score.js

--- HeroHealth/game/core/score.js
+++ HeroHealth/game/core/score.js
@@ -1,20 +1,24 @@
 // === core/score.js ===
-// คำนวณคะแนน + คอมโบ + bestCombo
 export class ScoreSystem {
-  constructor(){
+  constructor() {
     this.value = 0;
     this.combo = 0;
     this.bestCombo = 0;
   }
-  add(n = 0){
-    this.value += (n|0);
+  add(n = 0) {
+    this.value += (n | 0);
+    this.combo = (this.combo | 0) + (n > 0 ? 1 : 0);
+    if ((this.combo | 0) > (this.bestCombo | 0)) this.bestCombo = this.combo | 0;
   }
-  incCombo(){
-    this.combo = (this.combo|0) + 1;
-    if ((this.combo|0) > (this.bestCombo|0)) this.bestCombo = this.combo|0;
+  miss() {
+    this.combo = 0;
   }
-  resetCombo(){ this.combo = 0; }
-  get(){ return this.value|0; }
-  reset(){ this.value = 0; this.combo = 0; this.bestCombo = 0; }
+  reset() {
+    this.value = 0;
+    this.combo = 0;
+    this.bestCombo = 0;
+  }
+  get() {
+    return this.value | 0;
+  }
 }
-export default ScoreSystem;
