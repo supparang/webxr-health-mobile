ec5df366c58a16e105608bb9c6e9724b4d6a4160
supparang <supparang.r@chandra.ac.th>
2025-11-01 15:22:53 +0700

    Update vrinput.js

--- HeroHealth/game/core/vrinput.js
+++ HeroHealth/game/core/vrinput.js
@@ -1,8 +1,11 @@
 // === core/vrinput.js ===
+let _isXR = false;
+let _isGaze = false;
+
 export const VRInput = {
-  init(){ /* no-op */ },
-  toggleVR(){ /* no-op */ },
-  isXRActive(){ return false; },
-  isGazeMode(){ return false; },
+  init(){/* reserved */},
+  toggleVR(v){ _isXR = !!v; },
+  isXRActive(){ return !!_isXR; },
+  isGazeMode(){ return !!_isGaze; },
+  setGaze(v){ _isGaze = !!v; }
 };
-export default VRInput;
