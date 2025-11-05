eaea63ebb80a845a0caa6dae14f365a2fc8b38aa
supparang <supparang.r@chandra.ac.th>
2025-11-05 21:01:26 +0700

    Delete HeroHealth directory

--- HeroHealth/game/core/sfx.js
+++
@@ -1,5 +0,0 @@
-export class SFX{
-  static async init(){ return new SFX(); }
-  constructor(){ this.coach = new (await import('./coach.js')).Coach(); }
-  async play(name){ await this.coach.sfx(name); }
-}
