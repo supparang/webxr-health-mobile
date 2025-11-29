11973f3f6a26ab70d7ee6dbc1a106471c9b4777a
supparang <supparang.r@chandra.ac.th>
2025-11-29 22:22:01 +0700

    Update game-engine-goodjunk-vr.js

--- herohealth/vr/game-engine-goodjunk-vr.js
+++ herohealth/vr/game-engine-goodjunk-vr.js
@@ -3,7 +3,7 @@
 // ใช้ GameEngine เดิม แล้ว re-export เป็น GameEngineVR
 // เพื่อให้ goodjunk-vr.html import ได้ถูกชื่อ
 
-import { GameEngine } from './GameEngine.js';
+import { GameEngine } from './vr-goodjunk/GameEngine.js';
 
 // ใช้ชื่อ GameEngineVR ตามที่ goodjunk-vr.html เรียก
 export const GameEngineVR = GameEngine;
