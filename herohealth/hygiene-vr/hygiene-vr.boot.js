41299f72426d7fbdc2569b1f95447f2f47c6379d
supparang <supparang.r@chandra.ac.th>
2026-01-23 16:49:22 +0700

    Update hygiene-vr.boot.js

--- herohealth/hygiene-vr/hygiene-vr.boot.js
+++ herohealth/hygiene-vr/hygiene-vr.boot.js
@@ -1,3 +1,8 @@
 // === /herohealth/hygiene-vr/hygiene-vr.boot.js ===
+// Boot HygieneVR (bind UI + start engine)
 import { boot as engineBoot } from './hygiene.safe.js';
-(function(){ 'use strict'; engineBoot(); })();
+
+(function(){
+  'use strict';
+  engineBoot();
+})();
