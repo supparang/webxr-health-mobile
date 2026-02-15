0b68e53ef1a0b4d485f1eea03265f1632d652389
supparang <supparang.r@chandra.ac.th>
2026-02-15 16:22:10 +0700

    Update index.js

--- herohealth/api/index.js
+++ herohealth/api/index.js
@@ -1,5 +1,7 @@
 // === /herohealth/api/index.js ===
-// convenience exports
+// convenience exports (ESM barrel)
+
+'use strict';
 
 export * from './api-status.js';
 export * from './api-probe.js';
