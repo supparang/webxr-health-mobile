393bb51a557178e45d3dafd8b2e4ff31c3279631
supparang <supparang.r@chandra.ac.th>
2026-02-27 19:56:07 +0700

    Create firebase-config.js

---
+++ herohealth/battle/firebase-config.js
@@ -0,0 +1,10 @@
+// === /herohealth/battle/firebase-config.js ===
+'use strict';
+
+export const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
