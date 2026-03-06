4a1e80e858853eb51acb705fac592e8aa7a7b525
supparang <supparang.r@chandra.ac.th>
2026-03-06 21:28:02 +0700

    Update firebase-config.js

--- herohealth/firebase-config.js
+++ herohealth/firebase-config.js
@@ -1,10 +1,11 @@
 // === /herohealth/firebase-config.js ===
 window.HHA_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB5WmSR9uMYX2bwDh2iFYZwGglXGIq5Ijo",
+  authDomain: "herohealth-d7f8c.firebaseapp.com",
+  databaseURL: "https://herohealth-d7f8c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "herohealth-d7f8c",
+  storageBucket: "herohealth-d7f8c.firebasestorage.app",
+  messagingSenderId: "680817376848",
+  appId: "1:680817376848:web:1ed21b522b0703f6bd9b55",
+  measurementId: "G-T5J8DC0BKD"
 };
