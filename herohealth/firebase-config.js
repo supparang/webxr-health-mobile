0e075ab80f51cf02f0341320f3a32a91cd112f57
supparang <supparang.r@chandra.ac.th>
2026-03-27 18:02:36 +0700

    Update firebase-config.js

--- herohealth/firebase-config.js
+++ herohealth/firebase-config.js
@@ -1,9 +1,34 @@
-window.HHA_FIREBASE_CONFIG = {
-  apiKey: "PUT_YOUR_API_KEY_HERE",
-  authDomain: "PUT_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PUT_YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "PUT_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PUT_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PUT_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PUT_YOUR_APP_ID_HERE"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5WmSR9uMYX2bwDh2iFYZwGglXGIq5Ijo",
+  authDomain: "herohealth-d7f8c.firebaseapp.com",
+  databaseURL: "https://herohealth-d7f8c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "herohealth-d7f8c",
+  storageBucket: "herohealth-d7f8c.firebasestorage.app",
+  messagingSenderId: "680817376848",
+  appId: "1:680817376848:web:eed21b522b0703f6bd9b55",
+  measurementId: "G-T5J8DC0BKD"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Expose config for HeroHealth runtime
+window.HHA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB5WmSR9uMYX2bwDh2iFYZwGglXGIq5Ijo",
+  authDomain: "herohealth-d7f8c.firebaseapp.com",
+  databaseURL: "https://herohealth-d7f8c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "herohealth-d7f8c",
+  storageBucket: "herohealth-d7f8c.firebasestorage.app",
+  messagingSenderId: "680817376848",
+  appId: "1:680817376848:web:eed21b522b0703f6bd9b55",
+  measurementId: "G-T5J8DC0BKD"
+};
+
+window.HHA_FIREBASE_APP = app;
+window.HHA_FIREBASE_ANALYTICS = analytics;
