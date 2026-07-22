dcd91f4b0343cb60eda8d3cea46fa5af70e5337c
supparang <supparang.r@chandra.ac.th>
2026-07-22 21:44:12 +0700

    Enable HeroHealth classroom backend sync

--- HeroHealth_Learning1/data/backend-config.js
+++ HeroHealth_Learning1/data/backend-config.js
@@ -1,7 +1,7 @@
 window.HH_CONFIG = window.HH_CONFIG || {};
 window.HH_CONFIG.backend = {
-  enabled: false,
-  webAppUrl: "",
+  enabled: true,
+  webAppUrl: "https://script.google.com/macros/s/AKfycbwGTGnz1fg_bz4aBjvOZa7AbptWg6JXDDyF8I7cG7eIskjYZVAIoy148s7db5QmX-JP/exec",
   queueOffline: true,
   duplicateGuard: true,
   syncIntervalMs: 15000
