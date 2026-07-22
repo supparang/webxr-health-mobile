d7da3cee022ddb3548e728729ddab35ec32bf624
supparang <supparang.r@chandra.ac.th>
2026-07-22 21:38:35 +0700

    Add HeroHealth backend deployment configuration

---
+++ HeroHealth_Learning1/data/backend-config.js
@@ -0,0 +1,8 @@
+window.HH_CONFIG = window.HH_CONFIG || {};
+window.HH_CONFIG.backend = {
+  enabled: false,
+  webAppUrl: "",
+  queueOffline: true,
+  duplicateGuard: true,
+  syncIntervalMs: 15000
+};
