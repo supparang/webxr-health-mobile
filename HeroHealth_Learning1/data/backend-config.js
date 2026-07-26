86db52a45385531bf1d55c4b711f4ea96b2d2d71
supparang <supparang.r@chandra.ac.th>
2026-07-26 21:35:34 +0700

    Update backend-config.js

--- HeroHealth_Learning1/data/backend-config.js
+++ HeroHealth_Learning1/data/backend-config.js
@@ -1,11 +1,13 @@
 window.HH_CONFIG = window.HH_CONFIG || {};
+
 window.HH_CONFIG.backend = {
   enabled: true,
-  webAppUrl: "https://script.google.com/macros/s/AKfycbwGTGnz1fg_bz4aBjvOZa7AbptWg6JXDDyF8I7cG7eIskjYZVAIoy148s7db5QmX-JP/exec",
+  webAppUrl: "วาง_CLASSROOM_RECEIVER_EXEC_URL_ตรงนี้",
   queueOffline: true,
   duplicateGuard: true,
   syncIntervalMs: 15000
 };
+
 window.HH_CONFIG.teacherAccess = {
   sessionKey: "herohealth_teacher_authorized_v1",
   pin: "7319"
