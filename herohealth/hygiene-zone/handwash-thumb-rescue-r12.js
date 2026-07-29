440ce9cf1eb79ebfcafa3b6d00950a08111f56fa
supparang <supparang.r@chandra.ac.th>
2026-07-29 23:39:09 +0700

    Remove blocking document.write camera injection from Handwash startup

--- herohealth/hygiene-zone/handwash-thumb-rescue-r12.js
+++ herohealth/hygiene-zone/handwash-thumb-rescue-r12.js
@@ -1,10 +1,7 @@
 (() => {
   'use strict';
 
-  const RELEASE = '20260729-HANDWASH-SUMMARY-OVERLAY-FORCE-R22.1-CAMERA-BOOT';
-  if (!document.documentElement.dataset.handwashCameraPatch && document.readyState === 'loading') {
-    document.write('<script src="./handwash-camera-start-patch-r35.js?cv=20260729-HANDWASH-CAMERA-BOOT-R36"><\\/script>');
-  }
+  const RELEASE = '20260729-HANDWASH-SUMMARY-OVERLAY-FORCE-R22.2-STABLE-START';
   const NativeBlob = window.Blob;
 
   const rubHook = `function updateRub(phase,hands,dt){
