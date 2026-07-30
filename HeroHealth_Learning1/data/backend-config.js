4cbd69fcc09ecaea46fc1252d6d061a583e4b97a
supparang <supparang.r@chandra.ac.th>
2026-07-30 15:48:57 +0700

    fix(herohealth): point passport backend to current classroom receiver

--- HeroHealth_Learning1/data/backend-config.js
+++ HeroHealth_Learning1/data/backend-config.js
@@ -2,7 +2,7 @@ window.HH_CONFIG = window.HH_CONFIG || {};
 
 window.HH_CONFIG.backend = {
   enabled: true,
-  webAppUrl: "https://script.google.com/macros/s/AKfycbxU82Rg4KFStuZToOGlyX-rgzVkLpZ7yO1tW-gzui782eR7akes_HNZ5ec2TDUDh8J1/exec",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbwa-OSdqWS7uPne01wNr5a42PgKfAoxmUUm7yMcUx2D0C0OnbjrbppNUHkfjUxm79Fz/exec",
   queueOffline: true,
   duplicateGuard: true,
   syncIntervalMs: 15000
