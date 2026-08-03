dc62c4420279f7e76dc147c941263f6183601c19
supparang <supparang.r@chandra.ac.th>
2026-08-03 15:01:00 +0700

    feat(english-week): add passport configuration

---
+++ english-week-passport/config.js
@@ -0,0 +1,14 @@
+window.EW_CONFIG = Object.freeze({
+  appId: "ENGLISH-WEEK-PASSPORT-2026",
+  version: "2026-08-03-PASSPORT-V1",
+  webAppUrl: "",
+  defaultGroup: "English Week",
+  allowDemoWhenEndpointMissing: true,
+  requestTimeoutMs: 12000,
+  assessmentItems: 10,
+  leaderboardLimit: 10,
+  cacheKeys: Object.freeze({
+    identity: "ew_passport_identity_v1",
+    demoDb: "ew_passport_demo_db_v1"
+  })
+});
