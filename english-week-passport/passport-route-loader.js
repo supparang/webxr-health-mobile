75fffdfcf7f3b96a6cbf26d13639374586bec1df
supparang <supparang.r@chandra.ac.th>
2026-08-04 22:50:44 +0700

    chore(passport): load Lexicon X playable v5 route

--- english-week-passport/passport-route-loader.js
+++ english-week-passport/passport-route-loader.js
@@ -1,9 +1,9 @@
 (function(){
   "use strict";
-  const VERSION="2026-08-04-PASSPORT-ROUTE-LOADER-PLAYABLE4";
+  const VERSION="2026-08-04-PASSPORT-ROUTE-LOADER-PLAYABLE5";
   let loaded=false;
   const routes=[
-    "./word-match-route.js?v=20260804-playable4",
+    "./word-match-route.js?v=20260804-playable5",
     "./category-ar-route.js?v=20260803-category4",
     "./sentence-route.js?v=20260803-sentence2",
     "./action-detective-route.js?v=20260803-action5"
