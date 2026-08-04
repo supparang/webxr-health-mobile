ad24bff99380c9f89cc0f6fb0bc8bb2bea4fbc59
supparang <supparang.r@chandra.ac.th>
2026-08-04 14:00:57 +0700

    feat(herohealth): add Firebase config for Passport bridge v1

---
+++ HeroHealth_Learning1/firebase/firebase-config.js
@@ -0,0 +1,11 @@
+export const HEROHEALTH_FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyBdlWEf91s2gzUQf7H1pPB8c_hF807CpAc",
+  authDomain: "herohealth-learning.firebaseapp.com",
+  projectId: "herohealth-learning",
+  storageBucket: "herohealth-learning.firebasestorage.app",
+  messagingSenderId: "161380004818",
+  appId: "1:161380004818:web:7d8ef81c55eebd6b1a8e0b",
+  measurementId: "G-CF02ZSMM98"
+});
+
+export const HEROHEALTH_FIREBASE_BUILD = "2026-08-04-PASSPORT-BRIDGE-V1";
