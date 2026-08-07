649502e611291561260614624fbadae69cdf489d
supparang <supparang.r@chandra.ac.th>
2026-08-07 18:53:26 +0700

    English Week: configure Firebase Web app for direct Firestore

--- english-week-passport/firebase-web-config.js
+++ english-week-passport/firebase-web-config.js
@@ -1,6 +1,9 @@
 window.EW_FIREBASE_WEB_CONFIG = Object.freeze({
-  projectId: "englishweek-95869",
+  apiKey: "AIzaSyBjv6Aph2zxS41V7YTtSmkEAgIBBJfvleI",
   authDomain: "englishweek-95869.firebaseapp.com",
-  apiKey: "",
-  appId: ""
+  projectId: "englishweek-95869",
+  storageBucket: "englishweek-95869.firebasestorage.app",
+  messagingSenderId: "110133681532",
+  appId: "1:110133681532:web:fce7270a8838b03addd8f9",
+  measurementId: "G-3DCCL4D34V"
 });
