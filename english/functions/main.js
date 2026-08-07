8d76bc8c2d1904f5b8a498265658ccc8f9326d92
supparang <supparang.r@chandra.ac.th>
2026-08-07 12:25:06 +0700

    Export English Week journey authority

--- english/functions/main.js
+++ english/functions/main.js
@@ -1,3 +1,4 @@
 // Firebase Functions aggregate entry point
 export * from './index.js';
 export { englishWeekAuthority } from './english-week-authority.js';
+export { englishWeekJourney } from './english-week-journey.js';
