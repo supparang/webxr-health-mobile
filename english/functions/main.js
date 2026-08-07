8645f104eeead3944e3c48ee221ca3a06053e6ed
supparang <supparang.r@chandra.ac.th>
2026-08-07 13:56:11 +0700

    Export English Week teacher analytics function

--- english/functions/main.js
+++ english/functions/main.js
@@ -3,3 +3,4 @@ export * from './index.js';
 export { englishWeekAuthority } from './english-week-authority.js';
 export { englishWeekJourney } from './english-week-journey.js';
 export { englishWeekAssessmentCheckpoint } from './english-week-assessment-checkpoint.js';
+export { englishWeekTeacher } from './english-week-teacher.js';
