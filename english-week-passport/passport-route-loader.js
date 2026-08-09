a4c9b212cfb01718f48233907f1f474d4c8884ce
supparang <supparang.r@chandra.ac.th>
2026-08-09 14:39:46 +0700

    Bump Passport journey client to direct bootstrap V3

--- english-week-passport/passport-route-loader.js
+++ english-week-passport/passport-route-loader.js
@@ -1,11 +1,11 @@
 (function(){
   "use strict";
-  const VERSION="2026-08-09-PASSPORT-ROUTE-LOADER-V5-JOURNEY-DIRECT";
+  const VERSION="2026-08-09-PASSPORT-ROUTE-LOADER-V6-JOURNEY-DIRECT3";
   let loaded=false;
   const routes=[
     "./passport-canonical-routes-v1.js?v=20260809-route3-champion",
-    "./journey-client-v1.js?v=20260809-journey-direct2",
-    "./passport-journey-routes-v1.js?v=20260809-journey-direct2"
+    "./journey-client-v1.js?v=20260809-journey-direct3",
+    "./passport-journey-routes-v1.js?v=20260809-journey-direct3"
   ];
 
   function loadRoutes(){
